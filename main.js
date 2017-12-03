714d5ea87ab497629a663f76f72a6fce0fb72e37
fenghuan <fenghuan@mogoroom.com>
2017-12-03 16:58:56 +0800

    实现聊天室

---
+++ main.js
@@ -0,0 +1,37 @@
+var ws = require('nodejs-websocket');
+
+var game1 = null,
+    game2 = null,
+    game1Ready = false,
+    game2Ready = false;
+
+var connArray = [];
+
+var server = ws.createServer(function (conn) {
+  conn.on("text", function (str) {
+    console.log("收到的信息为:"+str)
+    if (str === 'apply-*-connect') {
+      conn.sendText("apply-*-success");
+      connArray.push(conn);
+    } else {
+      for (let i = 0; i < connArray.length; i++) {
+        let connObj = connArray[i];
+        if (connObj.readyState) {
+          connObj.sendText(str);
+        } else {
+          connArray.splice(i, 1);
+          i--;
+        }
+      }
+    }
+  })
+
+  conn.on("close", function (code, reason) {
+    console.log("关闭连接")
+  });
+  conn.on("error", function (code, reason) {
+    console.log("异常关闭")
+  });
+});
+
+server.listen(3200);
